dea60debb82b4480da5a6f3bb24313c4806b3c50
flexyw1be <geba_m@mail.ru>
2024-07-03 21:49:37 +0700

    123

--- main.cpp
+++ main.cpp
@@ -6,35 +6,55 @@
 using namespace std;
 
 
-void gotoxy(int x, int y) { 
-    COORD pos = {x, y};
-    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(output, pos);
-}
+// void gotoxy(int x, int y) { 
+//     COORD pos = {x, y};
+//     HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+//     SetConsoleCursorPosition(output, pos);
+// }
 
 const int WIDTH = 10;
 const int HEIGHT = 10;
+const int MENUPOINTS = 5;
+
+string Map[] = {
+"############\n",
+"#          #\n", 
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"############\n"};
+
+string MenuText[] = {
+    "Новая игра\n",
+    "Выбрать сложность\n",
+    "Рекорды\n",
+    "Выход\n",
+    "Уровень: "
+};
 
 
-char map[] = "##########\n"
-"#         #\n"
-"#         #\n"
-"#         #\n"
-"#         #\n"
-"#         #\n"
-"#         #\n"
-"#         #\n"
-"#         #\n"
-"###########\n";
+void menu(){
+    for(int i=0; i<MENUPOINTS; i++){
+        cout<<MenuText[i];
+    }
+}
+
 
 
 bool isRunning = true;
 int main(){
+    setlocale(0, ""); 
     int time = clock();
-
+    menu();
     // while(isRunning){
     //     cout<<"1";
+    //     for(int x =0; x < WIDTH; x++){
+    //         cout<<Map[x];
+    //     }
     // }
-    cout<<"1";
     return 0;
 }
