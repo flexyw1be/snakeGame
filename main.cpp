9c3a77ba16abc836d460af40c8adc9eed6b1103a
flexyw1be <geba_m@mail.ru>
2024-07-05 18:01:18 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,41 @@
 using namespace std;
 
 
+const int WIDTH = 10;
+const int HEIGHT = 10;
+const int MENUPOINTS = 5;
+string Map[] = {
+"############\n",
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"#          #\n",
+"############\n"};
+int level = 1;
+
+
+bool exxit = false;
+int currMenu = 0;
+int ch = 0;
+HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+string MenuText[] = {
+    "New game\n",
+    "Choice level\n",
+    "Records\n",
+    "Exit\n"};
+
+
+string LevelsText[] = {
+    "1\n",
+    "2\n",
+    "3\n"};
+
+
 void ClearScreen(){
     DWORD n;
     DWORD size;
@@ -19,14 +54,6 @@ void ClearScreen(){
     SetConsoleCursorPosition ( h, coord );
 }
 
-bool exxit = false;
-int currMenu = 0;
-int ch = 0;
-HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-string a = "New Game ";
-string b = "Choice level ";
-string c = "Records ";
-string d = "Exit ";
 
 
 void gotoxy(short x, short y)
@@ -34,15 +61,15 @@ void gotoxy(short x, short y)
     SetConsoleCursorPosition(hStdOut, {x, y});
 }
 
-void showMenu(){
+
+void setLevel(){
+    exxit = false;
     while (!exxit){
-        cout << a << endl;
-        cout << b << endl;
-        cout << c << endl;
-        cout << d << endl;
-
-        gotoxy(0, currMenu-1);
-
+        system("cls");
+         for(int i=0; i<size(LevelsText); i++){
+                cout<<LevelsText[i];
+         }
+        gotoxy(0, currMenu);
         ch = _getch();
         if (ch == 224){
             ch = _getch();
@@ -52,84 +79,70 @@ void showMenu(){
             case 72: currMenu--;    break;
             case 80: currMenu++;    break;
             case 13:
+                exxit = true;
+                system("cls");
                 if (currMenu == 0){
-                    system("cls");
+                    level = 1;
+                }
+                else if (currMenu == 1){
+                    level = 2;
+                }
+                else if (currMenu == 2){
+                    level = 3;
+                }
+            break;
+        }
+        if (currMenu < 0) currMenu = 0;
+        if (currMenu > 3) currMenu = 3;
+    }
+}
+void showMenu(){
+    while (!exxit){
+        system("cls");
+         for(int i=0; i<MENUPOINTS-1; i++){
+                cout<<MenuText[i];
+         }
+        gotoxy(0, currMenu);
+        ch = _getch();
+        if (ch == 224){
+            ch = _getch();
+        }
+        switch(ch){
+            case 27: exxit = true;   break;
+            case 72: currMenu--;    break;
+            case 80: currMenu++;    break;
+            case 13:
+                system("cls");
+                if (currMenu == 0){
                     cout << "idet igra" << endl;
                 }
                 else if (currMenu == 1){
-                    system("cls");
                     cout << "gg" << endl;
+                    setLevel();
+                    exxit = false;
                 }
                 else if (currMenu == 2){
-                    system("cls");
-                    exxit = true;
                     cout << "plaki-plaki" << endl;
                 }
             break;
         }
         if (currMenu < 0) currMenu = 0;
         if (currMenu > 3) currMenu = 3;
-
     }
 }
 
 
 
 
-const int WIDTH = 10;
-const int HEIGHT = 10;
-const int MENUPOINTS = 5;
-string Map[] = {
-"############\n",
-"#          #\n", 
-"#          #\n",
-"#          #\n",
-"#          #\n",
-"#          #\n",
-"#          #\n",
-"#          #\n",
-"#          #\n",
-"############\n"};
-string MenuText[] = {
-    "1> New game\n",
-    "2> Choice level\n",
-    "3> Records\n",
-    "4> Exit\n",
-    "Your level: "};
-int level = 1;
-
-
-// void menu(){
-//     ClearScreen();
-//     for(int i=0; i<MENUPOINTS-1; i++){
-//         cout<<MenuText[i];
-//     }
-//     cout<<MenuText[MENUPOINTS-1]<<level<<"\n";
-// }
-
-// void choiceLevel(){
-//     int lev;
-//     cin>>lev;
-//     level = lev;
-// }
-
-
-
 bool isRunning = true;
 int main(){
+    int n;
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     int time = clock();
     int choice;
     showMenu();
-    // menu();
-    // while(isRunning){
-    //     showMenu();
-    // }
-    //     cout<<"1";}
-    //     for(int x =0; x < WIDTH; x++){
-    //         cout<<Map[x];
-    //     }
-    // }
+    cin>>n;
+
     return 0;
 }
