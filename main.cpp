e94d58fc1ce393a09532ea734eff08252a0d6309
Mikhaylov Gleb <geba_m@mail.ru>
2024-07-07 01:24:49 +0700

    URAAAA ZMEYKAAAA

--- main.cpp
+++ main.cpp
@@ -21,10 +21,10 @@ const int RIGHT = 3;
 
 int snake_dir = UP;
 
-char snake = '0';
+char snake = 'o';
 int snake_x[MAX_LEN_SNAKE] = {0};
 int snake_y[MAX_LEN_SNAKE] = {0};
-int snake_len = 1;
+int snake_len = 2;
 
 int level = 1;
 bool exxit = false;
@@ -34,26 +34,26 @@ HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
 string Map[] = {
 "######################\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
-"#                    #\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
+"|                    |\n",
 "######################\n"
 };
 
@@ -149,17 +149,61 @@ void setLevel(){
     }
 }
 
+void checkKeys(){
+    if (GetKeyState('W') & 0x8000){
+            if(snake_dir != DOWN) snake_dir = UP;
+        }
+        if (GetKeyState('S') & 0x8000){
+            if(snake_dir != UP) snake_dir = DOWN;
+        }
+        if (GetKeyState('A') & 0x8000){
+            if(snake_dir != RIGHT) snake_dir = LEFT;
+        }
+        if (GetKeyState('D') & 0x8000){
+            if(snake_dir != LEFT) snake_dir = RIGHT;
+        }
+}
 
 void game(){
     snake_x[0] = WIDTH/2;
     snake_y[0] = HEIGHT/2;
+    srand((unsigned int)time(NULL));
+    int time = clock();
+    int food_x = 1 + (rand()%(WIDTH-3));
+    int food_y = 1 + (rand()%(HEIGHT-2));
+    char food = '*';
+
     while(isRunning){
         gotoxy(0, 0);
-        for(int i = 0; i < snake_len; i++){
-            Map[snake_y[i]][snake_x[i]] = snake;
+        checkKeys();
+        if(snake_x[0] == food_x && snake_y[0] == food_y){
+            food_x = 1 + (rand()%(WIDTH-3));
+            food_y = 1 + (rand()%(HEIGHT-2));
+            snake_len++;
         }
-        for(int i = 0; i < HEIGHT; i++){
-            cout<<Map[i];
+        if((clock() - time)* 4 / CLOCKS_PER_SEC >= 1){
+            time = clock();
+            if(snake_dir == UP) --snake_y[0];
+            if(snake_dir == DOWN) ++snake_y[0];
+            if(snake_dir == LEFT) --snake_x[0];
+            if(snake_dir == RIGHT) ++snake_x[0];
+            if(snake_x[0] == 0 || snake_x[0] == WIDTH-2 || snake_y[0] == 0 || snake_y[0] == HEIGHT-1) isRunning = false;
+
+            Map[food_y][food_x] = food;
+
+            for(int i = snake_len-2; i>= 0; i--){
+                snake_x[i+1] = snake_x[i];
+                snake_y[i+1] = snake_y[i];
+            }
+
+            for(int i = 0; i < snake_len; i++) Map[snake_y[i]][snake_x[i]] = snake;
+            Map[snake_y[0]][snake_x[0]] = '0';
+
+            for(int i = 0; i < HEIGHT; i++) cout<<Map[i];
+            cout<<"Lenght: "<<snake_len -1 ;
+
+            for(int i = 0; i < snake_len; i++) Map[snake_y[i]][snake_x[i]] = ' ';
+
         }
     }
 }
@@ -213,20 +257,17 @@ void showMenu(){
 int main(){
     HWND hwnd;
     char Title[1024];
-    GetConsoleTitle(Title, 1024); // Узнаем имя окна
-    hwnd=FindWindow(NULL, Title); // Узнаем hwnd окна
-    MoveWindow(hwnd,0,0,500,500,TRUE);//xnew,ynew,wnew,hnew -новые положение x,y, ширина и высота
+    GetConsoleTitle(Title, 1024);
+    hwnd=FindWindow(NULL, Title);
+    MoveWindow(hwnd,0,0,500,500,TRUE);
     HANDLE console_color;
-    console_color = GetStdHandle(
-        STD_OUTPUT_HANDLE);
 
-    SetConsoleTextAttribute(
-            console_color, 6);
-    int n;
+    console_color = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(console_color, 6);
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int time = clock();
-    int choice;
+
     showMenu();
     return 0;
 }
