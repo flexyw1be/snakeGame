fa60c31570269aafaf0f54074f148d19401113bb
Mikhaylov Gleb <geba_m@mail.ru>
2024-07-12 00:05:05 +0700

    ClASSSSIII

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include <fstream>
 using namespace std;
 
-//srand((unsigned int)time(NULL));
 
 
 
@@ -67,13 +66,14 @@ string LevelsText[] = {
     "3\n"
 };
 
+
+
+
 class Food{
 public:
      int x = 1 + (rand()%(WIDTH-3));
      int y = 1 + (rand()%(HEIGHT-2));
      char sign = 'F';
-
-
 };
 
 
@@ -84,6 +84,189 @@ public:
     int x[MAX_LEN_SNAKE] = {0};
     int y[MAX_LEN_SNAKE] = {0};
     int len = 2;
+
+};
+
+
+Snake checkKeys(Snake snake){
+    if (GetKeyState('W') & 0x8000){
+            if(snake.dir != DOWN) snake.dir = UP;
+        }
+        if (GetKeyState('S') & 0x8000){
+            if(snake.dir != UP) snake.dir = DOWN;
+        }
+        if (GetKeyState('A') & 0x8000){
+            if(snake.dir != RIGHT) snake.dir = LEFT;
+        }
+        if (GetKeyState('D') & 0x8000){
+            if(snake.dir != LEFT) snake.dir = RIGHT;
+        }
+    return snake;
+}
+
+
+
+class Game{
+public:
+    Snake snake;
+
+    void gotoxy(short x, short y){SetConsoleCursorPosition(hStdOut, {x, y});}
+
+    void showMenu(){
+        exxit = false;
+        while (!exxit){
+            system("cls");
+            for(int i=0; i<MENUPOINTS-1; i++)cout<<MenuText[i];
+            cout<<"Your level: "<<level;
+
+            gotoxy(0, currMenu);
+            ch = _getch();
+            if (ch == 224) ch = _getch();
+
+            switch(ch){
+                case 27: exxit = true;  system("cls");   break;
+                case 72: currMenu--;    break;
+                case 80: currMenu++;    break;
+                case 13:
+                    system("cls");
+                    if (currMenu == 0){
+                        cout << "idet igra" << endl;
+                        run();
+                    }
+                    if (currMenu == 1){
+                        cout << "gg" << endl;
+                        setLevel();
+                        exxit = false;
+                    }
+                    if (currMenu == 2){
+                        getRecords();
+                        exxit = false;
+                    }
+                    if (currMenu == 3){
+                        exxit = true;
+                    }
+                break;
+            }
+        if (currMenu < 0) currMenu = 0;
+        if (currMenu > 3) currMenu = 3;
+        }
+    }
+
+    void setLevel(){
+        exxit = false;
+        currMenu = 0;
+        while (!exxit){
+            system("cls");
+            for(int i=0; i<size(LevelsText); i++)cout<<LevelsText[i];
+            cout<<"Your level: "<<level;
+
+            gotoxy(0, currMenu);
+            ch = _getch();
+            if (ch == 224)ch = _getch();
+
+            switch(ch){
+                case 27: exxit = true;   break;
+                case 72: currMenu--;    break;
+                case 80: currMenu++;    break;
+                case 13:
+                    exxit = true;
+                    system("cls");
+                    if (currMenu == 0){
+                        level = 1;
+                        fin.close();
+                        fin.open("map1.txt");
+                    }
+                    if (currMenu == 1){
+                        level = 2;
+                        fin.close();
+                        fin.open("map2.txt");
+                    }
+                    if (currMenu == 2){
+                        level = 3;
+                        fin.close();
+                        fin.open("map3.txt");
+                    }
+                break;
+            }
+            if (currMenu < 0) currMenu = 0;
+            if (currMenu > 3) currMenu = 3;
+        }
+    }
+
+    void run(){
+        loadMap();
+        snake.x[0] = WIDTH/2;
+        snake.y[0] = HEIGHT/2;
+        int time = clock();
+        Food food;
+
+        while(isRunning){
+            gotoxy(0, 0);
+            snake = checkKeys(snake);
+            if(snake.x[0] == food.x && snake.y[0] == food.y){
+                food.x = 1 + (rand()%(WIDTH-3));
+                food.y = 1 + (rand()%(HEIGHT-2));
+                snake.len++;
+            }
+            if(snake.x[0] == WIDTH-1) snake.x[0] = 0;
+            if(snake.x[0] == 0) snake.x[0] = WIDTH-2;
+            if(Map[snake.y[0]][snake.x[0]] != ' '){isRunning = false;cout<<1;}
+            if((clock() - time)* 4 / CLOCKS_PER_SEC >= 1){
+                time = clock();
+                if(snake.dir == UP) --snake.y[0];
+                if(snake.dir == DOWN) ++snake.y[0];
+                if(snake.dir == LEFT) --snake.x[0];
+                if(snake.dir == RIGHT) ++snake.x[0];
+                if(Map[snake.y[0]][snake.x[0]] != ' ' && Map[snake.y[0]][snake.x[0]] != food.sign) isRunning = false;
+
+                Map[food.y][food.x] = food.sign;
+
+                for(int i = snake.len-2; i>= 0; i--){
+                    snake.x[i+1] = snake.x[i];
+                    snake.y[i+1] = snake.y[i];
+                }
+
+                for(int i = 0; i < snake.len; i++) Map[snake.y[i]][snake.x[i]] = snake.sign;
+                Map[snake.y[0]][snake.x[0]] = '0';
+
+                for(int i = 0; i < HEIGHT; i++) cout<<Map[i];
+                cout<<"Lenght: "<<snake.len -1 ;
+
+                for(int i = 0; i < snake.len; i++) Map[snake.y[i]][snake.x[i]] = ' ';
+
+            }
+        }
+        isRunning = true;
+        fin.close();
+        fin.open("map1.txt");
+        snake.len = 2;
+    }
+
+    void getRecords(){
+        exxit = false;
+         while (!exxit){
+            system("cls");
+            cout<<"Your records could be here";
+            ch = _getch();
+            if (ch == 224){
+                ch = _getch();
+            }
+            switch(ch){
+                case 13: exxit = true;
+                case 27: exxit = true;   break;
+                }
+
+            }
+    }
+
+    void loadMap(){
+        string str;
+        for(int i = 0; i < HEIGHT; i++){
+            getline(fin, str);
+            Map[i] = str + '\n';
+        }
+    }
+
 
 };
 
@@ -102,190 +285,6 @@ void ClearScreen(){
     SetConsoleCursorPosition ( h, coord );
 }
 
-void loadMap(){
-    string str;
-    for(int i = 0; i < HEIGHT; i++){
-        getline(fin, str);
-        Map[i] = str + '\n';
-    }
-}
-
-
-void gotoxy(short x, short y){
-    SetConsoleCursorPosition(hStdOut, {x, y});
-}
-
-
-void getRecords(){
-    exxit = false;
-     while (!exxit){
-        system("cls");
-        cout<<"Your records could be here";
-        ch = _getch();
-        if (ch == 224){
-            ch = _getch();
-        }
-        switch(ch){
-            case 13: exxit = true;
-            case 27: exxit = true;   break;
-            }
-
-        }
-}
-
-
-void setLevel(){
-    exxit = false;
-    currMenu = 0;
-    while (!exxit){
-        system("cls");
-        for(int i=0; i<size(LevelsText); i++){
-                cout<<LevelsText[i];
-        }
-        cout<<"Your level: "<<level;
-
-        gotoxy(0, currMenu);
-        ch = _getch();
-        if (ch == 224){
-                ch = _getch();
-        }
-        switch(ch){
-            case 27: exxit = true;   break;
-            case 72: currMenu--;    break;
-            case 80: currMenu++;    break;
-            case 13:
-                exxit = true;
-                system("cls");
-                if (currMenu == 0){
-                    level = 1;
-                    fin.close();
-                    fin.open("map1.txt");
-                }
-                else if (currMenu == 1){
-                    level = 2;
-                    fin.close();
-                    fin.open("map2.txt");
-                }
-                else if (currMenu == 2){
-                    level = 3;
-                    fin.close();
-                    fin.open("map3.txt");
-                }
-            break;
-        }
-        if (currMenu < 0) currMenu = 0;
-        if (currMenu > 3) currMenu = 3;
-    }
-}
-
-Snake checkKeys(Snake snake){
-    if (GetKeyState('W') & 0x8000){
-            if(snake.dir != DOWN) snake.dir = UP;
-        }
-        if (GetKeyState('S') & 0x8000){
-            if(snake.dir != UP) snake.dir = DOWN;
-        }
-        if (GetKeyState('A') & 0x8000){
-            if(snake.dir != RIGHT) snake.dir = LEFT;
-        }
-        if (GetKeyState('D') & 0x8000){
-            if(snake.dir != LEFT) snake.dir = RIGHT;
-        }
-    return snake;
-}
-
-void game(Snake snake){
-    loadMap();
-    snake.x[0] = WIDTH/2;
-    snake.y[0] = HEIGHT/2;
-    srand((unsigned int)time(NULL));
-    int time = clock();
-    Food food;
-
-    while(isRunning){
-        gotoxy(0, 0);
-        snake = checkKeys(snake);
-        if(snake.x[0] == food.x && snake.y[0] == food.y){
-            food.x = 1 + (rand()%(WIDTH-3));
-            food.x = 1 + (rand()%(HEIGHT-2));
-            snake.len++;
-        }
-        if(Map[snake.y[0]][snake.x[0]] != ' '){isRunning = false;cout<<1;}
-        if((clock() - time)* 4 / CLOCKS_PER_SEC >= 1){
-            time = clock();
-            if(snake.dir == UP) --snake.y[0];
-            if(snake.dir == DOWN) ++snake.y[0];
-            if(snake.dir == LEFT) --snake.x[0];
-            if(snake.dir == RIGHT) ++snake.x[0];
-            if(snake.x[0] == 0 || snake.x[0] == WIDTH-2 || snake.y[0] == 0 || snake.y[0] == HEIGHT-1) isRunning = false;
-
-            Map[food.y][food.x] = food.sign;
-
-            for(int i = snake.len-2; i>= 0; i--){
-                snake.x[i+1] = snake.x[i];
-                snake.y[i+1] = snake.y[i];
-            }
-
-            for(int i = 0; i < snake.len; i++) Map[snake.y[i]][snake.x[i]] = snake.sign;
-            Map[snake.y[0]][snake.x[0]] = '0';
-
-            for(int i = 0; i < HEIGHT; i++) cout<<Map[i];
-            cout<<"Lenght: "<<snake.len -1 ;
-
-            for(int i = 0; i < snake.len; i++) Map[snake.y[i]][snake.x[i]] = ' ';
-
-        }
-    }
-    isRunning = true;
-    fin.close();
-                    fin.open("map1.txt");
-}
-
-void showMenu(Snake snake){
-    exxit = false;
-    while (!exxit){
-        system("cls");
-         for(int i=0; i<MENUPOINTS-1; i++){
-                cout<<MenuText[i];
-        }
-        cout<<"Your level: "<<level;
-
-        gotoxy(0, currMenu);
-        ch = _getch();
-        if (ch == 224){
-            ch = _getch();
-        }
-        switch(ch){
-            case 27: exxit = true;  system("cls");   break;
-            case 72: currMenu--;    break;
-            case 80: currMenu++;    break;
-            case 13:
-                system("cls");
-                if (currMenu == 0){
-                    cout << "idet igra" << endl;
-                    game(snake);
-                }
-                else if (currMenu == 1){
-                    cout << "gg" << endl;
-                    setLevel();
-                    exxit = false;
-                }
-                else if (currMenu == 2){
-                    getRecords();
-                    exxit = false;
-                }
-                else if (currMenu == 3){
-                    exxit = true;
-                }
-            break;
-        }
-        if (currMenu < 0) currMenu = 0;
-        if (currMenu > 3) currMenu = 3;
-    }
-}
-
-
-
 
 int main(){
     HWND hwnd;
@@ -294,15 +293,16 @@ int main(){
     hwnd=FindWindow(NULL, Title);
     MoveWindow(hwnd,0,0,500,500,TRUE);
     HANDLE console_color;
-
+    srand((unsigned int)time(NULL));
     console_color = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(console_color, 6);
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+
+system("color 8E");
     fin.open("map1.txt");
+    Game game;
+    game.showMenu();
 
-        Snake snake;
-
-    showMenu(snake);
     return 0;
 }
